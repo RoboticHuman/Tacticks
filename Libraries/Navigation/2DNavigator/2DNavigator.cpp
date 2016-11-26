d3b9129601d3ea935814b081ab59e3ba99a4c0dc
RoboticHuman <donomar@aucegypt.edu>
2016-11-26 12:13:23 +0200

    fixed 2DNavigator example and added missing externs

--- Libraries/Navigation/2DNavigator/2DNavigator.cpp
+++ Libraries/Navigation/2DNavigator/2DNavigator.cpp
@@ -1,38 +1,55 @@
 #include "2DNavigator.h"
+#include "Tacticks/Navigation.h"
 #include <algorithm>
-void 2DNavigator::init(std::vector<PassObject> args){
-  PassArray grid = object.asArray();
-  for (int i=0;i<grid.size();i++)
-  {
-    vector<bool> row;
-    for(int j=0;j<grid[i].size();j++)
+void Navigator2D::init(std::vector<PassObject> args){
+  if (args.size()==0) fprintf(stdout, "%s\n","Args to 2DNavigator is empty :(");
+  else{
+    PassArray grid = args[0].asArray();
+    for (int i=0;i<grid.size();i++)
     {
-      row.push_back((bool)grid[i][j]);
+      std::vector<bool> row;
+      for(int j=0;j<grid[i].asArray().size();j++)
+      {
+        row.push_back((bool)(grid[i].asArray()[j]).asInteger());
+      }
+      navGrid.push_back(row);
     }
-    grid.push_back(row);
-  }
+    initialized = true;
+}
 }
 
-std::vector<PassObject> 2DNavigator::getPossibleNextLocations(int x, int y){
+std::vector<PassObject> Navigator2D::getPossibleNextLocations(int x, int y){
   int di[]={1,-1,0,1};
   int dj[]={0,0,1,-1};
   std::vector<PassObject> tempVec;
-  for(int i=0;<4;i++)
-  {
-    PassArray coordinate(2);
-    int newX = x+di[i];
-    int newY = y+dj[j];
-    if(newX>=0 && newX<grid[0].size() && newY>=0 && newY<grid[0].size() && grid[newX][newY]!=1){
-      coordinate[0]=newX; coordinate[1]=newY;
-      tempVec.push_back(PassObject("coordinate",coordinate));
+  if(initialized){
+    for(int i=0;i<4;i++)
+    {
+      PassArray coordinate(2);
+      int newX = x+di[i];
+      int newY = y+dj[i];
+      if(newX>=0 && newX<navGrid[0].size() && newY>=0 && newY<navGrid[0].size() && navGrid[newX][newY]!=1){
+        coordinate[0]=PassObject("x",newX); coordinate[1]=PassObject("y",newY);
+        tempVec.push_back(PassObject("coordinate",coordinate));
+      }
     }
-  }
-  std::random_shuffle(tempVec.begin(),tempVec.end());
+    std::random_shuffle(tempVec.begin(),tempVec.end());
+}
   return tempVec;
 }
 
-std::vector<PassObject> 2DNavigator::getData(std::string dataName, std::vector<PassObject> args){
+std::vector<PassObject> Navigator2D::getData(std::string dataName, std::vector<PassObject> args){
   if(dataName=="OneLove"){
     return getPossibleNextLocations(args[0].asInteger(),args[1].asInteger());
   }
+}
+
+extern "C"
+AbstractNavigation* newNav(){
+  return new Navigator2D;
+}
+
+extern "C"
+NavigationInfo declareDependencies(){
+  return {{}};
 }
--- Libraries/Navigation/2DNavigator/2DNavigator.h
+++ Libraries/Navigation/2DNavigator/2DNavigator.h
@@ -1,15 +1,14 @@
 #pragma once
-#include "Tacticks/AbstractNavigation"
+#include "Tacticks/AbstractNavigation.h"
 #include "Tacticks/PassObject.h"
 #include <vector>
-class 2DNavigator : public AbstractNavigation{
+class Navigator2D : public AbstractNavigation{
 private:
   PassObject passObject;
-  std::vector<std::vector<bool>> grid;
-  std::vector<PassObject> getPossibleNextLocations();
+  std::vector<std::vector<bool>> navGrid;
+  std::vector<PassObject> getPossibleNextLocations(int x, int y);
+  bool initialized=false;
 public:
-  2DNavigator();
-  ~2DNavigator ();
   virtual void init(std::vector<PassObject> args) override;
   virtual std::vector<PassObject> getData(std::string dataName, std::vector<PassObject> args) override;
 };
